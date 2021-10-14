6aae011efa8ffc39a9e646ac4754fc8bc7540362
Chaika <ab150978cav@gmail.com>
2021-10-14 23:23:21 +0300

    Chaika commit hw2

---
+++ Chaika/HW1/hw1_Task1.py
@@ -0,0 +1,24 @@
+#       Task 1
+#   Define variables a and b. Read values a and b from console and calculate: 
+#   Определите переменные a и b. Посчитайте значения a и b в консоли :
+#    a + b 
+#    a - b 
+#    a * b 
+#    a / b. 
+#    Output obtained results.
+
+a = int(input('input A:'))
+b = int(input('input B:'))
+print(' ')      
+print('Task1')
+print(' ')
+print('a + b = ', a + b)
+#print(a + b)
+print('a - b = ', a - b)
+#print(a - b)
+print('a * b = ', a * b)
+#print(a * b)
+print('a / b = ', a / b)
+#print(a / b)
+
+
