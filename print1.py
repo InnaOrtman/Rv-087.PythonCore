43f1f48916a514c3b95bcff9dfa4598d38c428b7
Roma <kolbektriora@gmail.com>
2021-10-15 15:09:11 +0300

    New files

---
+++ print1.py
@@ -0,0 +1,7 @@
+a = int(input('Enter value of a: '))
+b = int(input('Enter value of b: '))
+
+print(f'a + b = {a + b}')
+print(f'a - b = {a - b}')
+print(f'a * b = {a * b}')
+print(f'a / b = {a / b}') 
---
+++ print2.py
@@ -0,0 +1,5 @@
+def summa2 (a, b):
+    return a+b
+ab = summa2 (10, 20)
+# print('Сумма а+б=',ab)
+print(f'Сумма а+б={ab}')
