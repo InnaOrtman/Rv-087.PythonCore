e7b6d925c55cdd2f18db6d1cf3e264f87e10dbda
EasternCheesE <easterntwelve@gmail.com>
2021-10-15 17:04:27 +0300

    [main_KirillP] deleting Homework folder

--- Homework/Task_2_p2.py
+++
@@ -1,5 +0,0 @@
-name = raw_input("What is your name?\n")
-age = raw_input("How old are you?\n")
-city = raw_input("Where do you live?\n")
-
-print "Hello, {}. Your age is {}. You live in {}.\n".format(name, age, city)
