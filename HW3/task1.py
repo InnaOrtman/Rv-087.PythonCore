25e0d059da219a521edf36ad40b5e53d2f2ef981
EasternCheesE <easterntwelve@gmail.com>
2021-10-16 12:13:52 +0300

    [kirillP_HW3] added homework No. 3

--- HW3/task1.py
+++ HW3/task1.py
@@ -1,1 +1,9 @@
-task1_py
+a = int(input("Please enter value for a\n"))
+b = int(input("Please enter value for b\n"))
+
+print(f"Your entered value for a = {a}, b = {b}")
+a = a + b
+b = a - b
+a = a - b
+
+print(f"Now, a = {a}, b = {b}")
---
+++ HW3/task4.py
@@ -0,0 +1,6 @@
+radius = float(input("Please enter radius\n"))
+
+PI = 3.14
+area = PI * pow(radius, 2)
+
+print(f"Circle area = {area}") 
