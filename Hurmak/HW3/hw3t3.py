f587f83105f54c8dbada2a5480474752c5ad5c48
HurmakR <r.hurmak@gmail.com>
2021-10-19 00:18:01 +0300

    homework for l3 completed, 6 files added

---
+++ Hurmak/HW3/hw3t3.py
@@ -0,0 +1,5 @@
+width = float(input('Enter rectangular width: '))
+height = float(input('Enter rectangular height: ')) 
+area = width * height
+perimeter = (width + height) * 2
+print(f'Area = {area}, perimeter = {perimeter}')
---
+++ Hurmak/HW3/hw3t4.py
@@ -0,0 +1,5 @@
+import math
+radius = float(input('Enter radius of the circle: '))
+area = math.pi * radius ** 2
+circumference = 2 * math.pi * radius
+print(f'Radius={radius}, area={area}, circumference={circumference}') 
