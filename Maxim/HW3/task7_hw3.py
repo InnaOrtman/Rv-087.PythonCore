ed071587e32961f7a113ae12eeccff774fa779c6
Maxim <limichmaxim@gmail.com>
2021-10-18 16:20:51 +0300

    HW3 Task7

---
+++ Maxim/HW3/task7_hw3.py
@@ -0,0 +1,16 @@
+# Task 7**
+# Please suggest your option of situation where different data types
+# are used for saving the information
+# (describe situation like in previous task).
+
+# player1 = ("Name",
+#            "Surname",
+#            {"age": int,
+#             "height": int,
+#             "weight": float},
+#            bool,  #Main cast
+#            ["list of wins", ]
+#            )
+sport_team = ([player1, ],
+              "What kind of sport",
+              {"turnament": result, })
