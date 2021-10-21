22619705001b251cefa6f9bd5118f0a7a211d964
yuriy <yuriymuravskyy@gmail.com>
2021-10-22 01:11:09 +0300

    modify task6

--- muravskyy/hw4/ht6.py
+++ muravskyy/hw4/ht6.py
@@ -1,17 +1,22 @@
+import math
+
 ticket = int(input('choose your seat (available up 54)\t'))
 
 
-seat = ''
 if ticket % 2 != 0:
     if ticket < 37:
+        c = math.ceil(ticket / 4)
         seat = ('botton and compartment')
+        print()
     else:
-        seat = ('botton and outside')    
+        seat = ('botton and outside') 
+        c = ''  
 else:
     if ticket < 37:        
         seat = ('top and compartment')
+        c = math.ceil(ticket / 4)
     else:
         seat = ('top and outside') 
-
+        c = ''
 seat = 'your place is '+seat        
-print(seat)
+print(seat,c)
