343bb7969a531c0b0d20893ca8e3a6da2ee6a652
RuslanaPereviznyk <92511257+RuslanaPereviznyk@users.noreply.github.com>
2021-10-21 20:09:44 +0200

    message

---
+++ Ruslana_Pereviznyk/hw2/hw1.2.py
@@ -0,0 +1,14 @@
+x='What is your name?'
+print(x)
+y="My name is Ruslana"
+print (y)
+z=x+" "+y
+print(z)
+a=" Where do you live?"
+b="I live in Ukraine"
+c=a + b
+print (c)
+d="How old are you?"
+e="I am 35 years old"
+f=d+e
+print(f)
