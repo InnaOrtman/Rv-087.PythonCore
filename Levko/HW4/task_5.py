ae4af419d8a2e495de7cbc0aaf53b13032ff4134
InnaOrtman <innalevko1987@gmail.com>
2021-10-22 17:51:00 +0300

    Update task_5.py

--- Levko/HW4/task_5.py
+++ Levko/HW4/task_5.py
@@ -2,30 +2,20 @@
 
 money = int(input("Enter the amount of money (UAH): "))
 
-if money % 200 == 0:
-    print(f"You will have: {round(money/200)} 200/banknotes")
-elif money < 200:
+if money < 200:
     print(f"You will have: 0 200/banknotes")
-elif money % 200 != 1:
-    print(f"You will have: {round(money/200)-1} 200/banknotes")
+else:
+    print(f"You will have: {int( money / 200)} 200/banknotes")
 
-if money % 100 == 0:
-    print(f"You will have: {round(money/100)} 100/banknotes")
-elif money < 100:
+if money < 100:
     print(f"You will have: 0 100/banknotes")
-elif money % 100 != 1:
-    print(f"You will have: {round(money/100)-1} 100/banknotes")
-elif money == money:
-    print(f"You will have: {money}  1/banknotes")
+else:
+    print(f"You will have: {int( money / 100)} 100/banknotes")
 
-if money % 10 == 0:
-    print(f"You will have: {round(money/10)} 10/banknotes")
-elif money < 10:
+if money < 10:
     print(f"You will have: 0 10/banknotes")
-elif money % 10 != 1:
-    print(f"You will have: {round(money/10)-1} 10/banknotes")
+else:
+    print(f"You will have: {int( money / 10)} 10/banknotes")
 
 if money == money:
     print(f"You will have: {money}  1/banknotes")
-
-
