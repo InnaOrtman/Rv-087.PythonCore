82442436059db9a74bb4d2b679795e5c7850d201
Maxim <limichmaxim@gmail.com>
2021-10-22 14:03:12 +0300

    Done task5 HW4

---
+++ Maxim/HW4/task5_hw4.py
@@ -0,0 +1,40 @@
+# Task 5
+# The amount of money is known.
+# Exchange it with 200, 100, 10 banknotes and 1 UAH coin, if possible
+# TODO: make a better output
+
+import parrPathApp
+from mynum import is_positive_natural
+from mynum import if_int_to_int
+
+
+parrPathApp.run()
+
+
+def main():
+    balBanknotes = {"200": 0,
+                    "100": 0,
+                    "10": 0,
+                    "1": 0}
+
+    money = input("Enter amount of money: ")
+    if not is_positive_natural(money):
+        print("You entered not a value of money or "
+              "not integer amount of money")
+        return -1
+
+    money = float(money)
+
+    for item in balBanknotes.items():
+        moneyDiv = money // float(item[0])
+
+        if moneyDiv != 0:
+            balBanknotes[item[0]] = if_int_to_int(moneyDiv)
+
+        money = int(money) - int(moneyDiv) * int(item[0])
+
+    print(balBanknotes)
+
+
+if __name__ == "__main__":
+    main()
