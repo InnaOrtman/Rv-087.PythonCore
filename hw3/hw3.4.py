bdee5689411a5e202bfd2046f91bb6c57030ff37
RuslanaPereviznyk <92511257+RuslanaPereviznyk@users.noreply.github.com>
2021-10-25 20:50:57 +0200

    message

---
+++ hw3/hw3.4.py
@@ -0,0 +1,7 @@
+r=int(input('vvedy radius'))
+import math
+p=math.pi
+l=2*p*r
+s=p*(r**2)
+print ("довжина кола", l)
+print ( "площа круга", s)
---
+++ hw3/hw3.5.py
@@ -0,0 +1,4 @@
+a = int(input('vvedy 1 katat'))
+b = int(input('vvedy 2 katet'))
+x=( a**2 + b**2)**(1/2)
+print ( "гіпотенуза прямокутного трикутника" , x )
