52c826590d1752e985f0bf75b5fb731ad2adf7dd
Maxim <limichmaxim@gmail.com>
2021-10-27 14:18:56 +0300

    Done task 1 HW5

---
+++ Maxim/HW5/task1_hw5.py
@@ -0,0 +1,36 @@
+# Task 1
+# The program generates a random integer from 0 to 100.
+# The user must guess it in no more than 10 attempts.
+# After each unsuccessful attempt, program says if the number entered
+# by user more or less than required number. If the number is not
+# guessed for 10 attempts, then display the guessed number.
+
+import random
+from mynum import is_positive_natural
+
+
+hiddenNumber = random.randrange(0, 101)
+i = 1
+win = False
+
+print("Guess the hidden number from 0 to 100. You have 10 attempts.")
+while i < 11:
+    guess = input(f"Enter the number. Attempt {i}/10: ")
+
+    if not is_positive_natural(guess):
+        print("You enter not a number. You attempt won't be count.")
+        continue
+    elif int(guess) != hiddenNumber:
+        if int(guess) > hiddenNumber:
+            print("The number is less")
+        else:
+            print("The number is more")
+        i += 1
+    else:
+        win = True
+        break
+
+if not win:
+    print(f"You lost the game. The hidden number was {hiddenNumber}")
+else:
+    print(f"You won! The hidden number was {hiddenNumber}")
