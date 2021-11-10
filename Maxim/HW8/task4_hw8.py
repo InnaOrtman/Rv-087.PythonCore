e9b37ffe0ba3f7b36dc6c93826f2451294beafbc
Maxim <limichmaxim@gmail.com>
2021-11-10 14:17:22 +0200

    Done task 4 HW8

---
+++ Maxim/HW8/task4_hw8.py
@@ -0,0 +1,24 @@
+# Task 4
+# Create the function is_prime, which takes 1 argument - a number from
+# 0 to 1000, and returns True if it is simple, and False - otherwise.
+
+from mynum import is_positive_natural
+
+
+def isPrime(number: int) -> bool:
+    if number > 1:
+        for i in range(2, number-1):
+            if number % i == 0:
+                return False
+            return True
+    return False
+
+
+while True:
+    num = input("Enter the number: ")
+    if not is_positive_natural(num):
+        print("You entered incorrect value")
+        continue
+    break
+
+print(isPrime(int(num)))
