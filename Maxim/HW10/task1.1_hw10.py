cd6615b01c70b703dc4095502659a8b8e93b32e6
Maxim <limichmaxim@gmail.com>
2021-11-19 12:10:30 +0200

    Done task 1 HW 10

---
+++ Maxim/HW10/task1.1_hw10.py
@@ -0,0 +1,7 @@
+import calculator.calc as calc
+
+
+print(calc.mySum(1, 2, 1.1, 0.9))
+print(calc.myDiff(10, 2, 3, -3, 1.1, 0.9))
+print(calc.myProd(1, 2.1))
+print(calc.myFraction(1, 5, 7, 10, 5, 2.2))
---
+++ Maxim/HW10/task1.3_hw10.py
@@ -0,0 +1,7 @@
+from calculator.calc import *
+
+
+print(mySum(1, 2, 1.1, 0.9))
+print(myDiff(10, 2, 3, -3, 1.1, 0.9))
+print(myProd(1, 2.1))
+print(myFraction(1, 5, 7, 10, 5, 2.2))
