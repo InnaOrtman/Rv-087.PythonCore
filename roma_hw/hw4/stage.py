3316281ecd19228925bbe94f4c2cac1c19cf1fa4
Romaaashka <91368360+Romaaashka@users.noreply.github.com>
2021-12-03 00:27:09 +0200

    home work4 added

---
+++ roma_hw/hw4/stage.py
@@ -0,0 +1,11 @@
+import random
+S=random.randint(10,100)
+print("S="+str(S))
+R=random.randint(10,100)
+print("R="+str(R))
+K=random.randint(1,10)
+print("K="+str(K))
+if S-K>R :
+    print("In the hall you can put the scene")
+if S-K<=R:
+    print("In the hall it is impossible to place a scene")
